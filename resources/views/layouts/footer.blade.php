c6bb0d299408a83bab7ec14d3195cbd763053cf6
richard4s <richard.oluwo@imaxinacion.info>
2018-12-31 20:31:48 +0100

    Our desired landing page

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: richard
+ * Date: 12/31/18
+ * Time: 7:16 PM
+ */
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: richard
+ * Date: 12/31/18
+ * Time: 7:15 PM
+ */
