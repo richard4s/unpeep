4e76a8429e4db0d57a0a3b0f5b126ef399584003
richard4s <richard.oluwo@imaxinacion.info>
2019-01-03 19:32:43 +0100

    Added hotjar tracking code in book photographer page

--- resources/views/book_photographer.blade.php
+++ resources/views/book_photographer.blade.php
@@ -78,8 +78,19 @@
 <!-- JS -->
 <script src="{{ URL::asset('assets/vendor/jquery/jquery.min.js') }}"></script>
 <script src="{{ URL::asset('assets/vendor/jquery-validation/dist/jquery.validate.min.js') }}"></script>
-<script src="{{ URL::asset('assets/endor/jquery-validation/dist/additional-methods.min.js') }}v"></script>
+<script src="{{ URL::asset('assets/vendor/jquery-validation/dist/additional-methods.min.js') }}v"></script>
 <script src="{{ URL::asset('assets/vendor/jquery-steps/jquery.steps.min.js') }}"></script>
 <script src="{{ URL::asset('assets/js/main.js') }}"></script>
+<!-- Hotjar Tracking Code for www.unpeep.com -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1143508,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
 </body>
 </html>
