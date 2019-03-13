cc65cc77aedf8f3613999349f894e1b83f9e51b1
richard4s <richardoluwo50@gmail.com>
2019-03-13 04:24:08 +0100

    Right session class

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     @if (session('status'))
         <div class="alert alert-success" role="alert">
-            {{  session::flash('status') }}
+            {{  \Session::flash('status') }}
         </div>
     @endif
 
