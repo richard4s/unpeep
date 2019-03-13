f5b88154dec856a2cfee0bc7778153138fbf883e
richard4s <richardoluwo50@gmail.com>
2019-03-13 04:30:19 +0100

    basically a revert

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     @if (session('status'))
         <div class="alert alert-success" role="alert">
-            {{  \Session::flash('status') }}
+            {{ session('status') }}
         </div>
     @endif
 
