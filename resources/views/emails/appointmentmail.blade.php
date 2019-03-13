b40df748551d5052416bba5a9b471b1b1ecabbc7
richard4s <richardoluwo50@gmail.com>
2019-03-13 06:41:04 +0100

    The s

--- resources/views/emails/appointmentmail.blade.php
+++ resources/views/emails/appointmentmail.blade.php
@@ -1,25 +1,25 @@
-<div style="margin:50px auto;text-align: center;">
-    <a href="{{ env('APP_URL') }}" target="_blank">{{ env('APP_NAME') }}</a>
-    <p>Hello, {{ $firstName }}</p>
-    <p> Booked an appointment with a Peeper.<br><br>
+{{--<div style="margin:50px auto;text-align: center;">--}}
+    {{--<a href="{{ env('APP_URL') }}" target="_blank">{{ env('APP_NAME') }}</a>--}}
+    {{--<p>Hello, {{ $firstName }}</p>--}}
+    {{--<p> Booked an appointment with a Peeper.<br><br>--}}
 
-    You booked an appointment with a peeper.<br>
-    Your appointment details are:
-        <ul>
-            <li>Fullname: {{ $firstName.' '. $lastName }}</li>
-            <li>Email: {{ $email }}</li>
-            <li>Telephone: {{ $telephone }}</li>
-            <li>Venue: {{ $venue }}</li>
-            <li>Date: {{ $dates }}</li>
-            <li>Time: {{ $time }}</li>
-            <li>No. Of Photographers: {{ $adults }}</li>
-            <li>Event: {{ $event }}</li>
-            <li>Notes: {{ $note }}</li>
-        </ul>
-    </p>
-    <br>
-    <p>Thanks for working with Unpeep.</p>
-</div>
+    {{--You booked an appointment with a peeper.<br>--}}
+    {{--Your appointment details are:--}}
+        {{--<ul>--}}
+            {{--<li>Fullname: {{ $firstName.' '. $lastName }}</li>--}}
+            {{--<li>Email: {{ $email }}</li>--}}
+            {{--<li>Telephone: {{ $telephone }}</li>--}}
+            {{--<li>Venue: {{ $venue }}</li>--}}
+            {{--<li>Date: {{ $dates }}</li>--}}
+            {{--<li>Time: {{ $time }}</li>--}}
+            {{--<li>No. Of Photographers: {{ $adults }}</li>--}}
+            {{--<li>Event: {{ $event }}</li>--}}
+            {{--<li>Notes: {{ $note }}</li>--}}
+        {{--</ul>--}}
+    {{--</p>--}}
+    {{--<br>--}}
+    {{--<p>Thanks for working with Unpeep.</p>--}}
+{{--</div>--}}
 
 
 
@@ -139,7 +139,7 @@
                                                         <li>Time: {{ $time }}</li>
                                                         <li>No. Of Photographers: {{ $adults }}</li>
                                                         <li>Event: {{ $event }}</li>
-                                                        <li>Notes: {{ $note }}</li>
+                                                        <li>Notes: {{ $notes }}</li>
                                                     </ul>
                                                     </p>
                                                 </td>
